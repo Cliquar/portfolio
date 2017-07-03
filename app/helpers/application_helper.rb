3146ca4ba177235968e2bf79d99fa1bfec1e03b7
Cliquar <postitherepls@aol.de>
2017-07-03 16:34:25 +0200

    made static pages and build navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+
+  def nav_items
+    [
+      {url: root_path,
+      title: 'Home'},
+      {url: about_path,
+      title: 'About'},
+      {url: contact_path,
+      title: 'Contact'},
+    ]
+  end
+
+  def nav_helper
+    nav_links = ''
+
+    nav_items.each do |item|
+      nav_links << "<li class='nav-item'><a href='#{item[:url]}' class='nav-link'>#{item[:title]}</a></li>"
+    end
+    nav_links.html_safe
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
-  root to: 'pages/home'
-  get 'pages/about'
+  root to: 'pages#home'
+  get 'about', to: 'pages#about'
 
-  get 'pages/contact'
+  get 'contact', to: 'pages#contact'
 
-  get 'home/about'
-
-  get 'home/contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
