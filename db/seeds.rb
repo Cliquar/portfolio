8b3927be71952967b609c45fdecc751082adadd4
Cliquar <postitherepls@aol.de>
2017-07-06 23:13:13 +0200

    changed seeds file for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,3 @@
-10.times do |f|
-  Portfolio.create!(
-            title: "Portfolio No #{f}",
-            sub_title: "A project about #{f}",
-            description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, 
-                          sed diam nonumy eirmod tempor invidunt ut labore et dolore magna 
-                          aliquyam erat, sed diam voluptua. At vero eos et accusam et justo 
-                          duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus 
-                          est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, 
-                          consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut 
-                          labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et 
-                          accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, 
-                          no sea takimata sanctus est Lorem ipsum dolor sit amet.",
-            thumb_img: "http://via.placeholder.com/242x160",
-            main_img: "http://via.placeholder.com/600x450",
-            gh_link: "https://github.com/Cliquar/portfolio" , 
-            url_link: "https://www.google.de/"             
-                          )
-end
 
 Training.create!(
   topic: "Web Development",
