7bcb040e4ffc696e185c78178c546b5b749a4fb0
Cliquar <postitherepls@aol.de>
2017-07-06 22:17:54 +0200

    added carreierwave with aws. Images are now hosted on aws.

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,6 +1,9 @@
 class Portfolio < ApplicationRecord
   validates :title, presence:true
 
+  mount_uploader :thumb_img, PortfolioUploader
+  mount_uploader :main_img, PortfolioUploader
+
   def self.latest 
     order("created_at DESC").limit(4)
   end
